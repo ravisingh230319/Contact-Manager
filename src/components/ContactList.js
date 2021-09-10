91d99435e47a3909358b5cf5bc1b1ebe201b6dac
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-10 09:00:16 +0530

    Using components

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,13 @@
-function ContactList(props) {
+function ContactList({contacts, onEdit, onDelete}) {
 
     return (
         <div className="list">
-            <label>{props.contacts.name}</label>
-            <label style={{ marginLeft: "40px" }}>{props.contacts.email}</label>
-            <button className="btn-edit" onClick={() => { props.onEdit(props.contacts) }}>
+            <label>{contacts.name}</label>
+            <label style={{ marginLeft: "40px" }}>{contacts.email}</label>
+            <button className="btn-edit" onClick={() => { onEdit(contacts) }}>
                 Edit
             </button>
-            <button className="btn-delete" onClick={() => { props.onDelete(props.contacts.id) }}>
+            <button className="btn-delete" onClick={() => { onDelete(contacts.id) }}>
                 Delete
             </button>
             <br />
